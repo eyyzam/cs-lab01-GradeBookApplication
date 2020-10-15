f24c622a6a78bfaa80f5f39f3fff009a97908526
eyyzam <bronislawskiandrzej@gmail.com>
2020-10-15 23:07:00 +0200

    STEP - Override RankedGradeBook's GetLetterGrade method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,7 @@
-﻿using GradeBook.Enums;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +10,32 @@ namespace GradeBook.GradeBooks
 		public RankedGradeBook(string name) : base(name)
 		{
 			Type = GradeBookType.Ranked;
+		}
+
+		public override char GetLetterGrade(double averageGrade)
+		{
+			if (Students.Count < 5) throw new InvalidOperationException("At least 5 students needed to perform ranked grading!");
+
+			// Using LINQ to sort the list by descending on "AverageGrade"
+			// and making a list containing only AverageGrades of all students
+			var studentGrades = Students.OrderByDescending(x => x.AverageGrade).Select(x => x.AverageGrade).ToList();
+
+			// Obtaining one fifth of the students count
+			var oneFifth = (int)Math.Ceiling(Students.Count * 0.2);
+
+			// List of obtainable grades
+			var obtainableGrades = new List<char>() {'A', 'B', 'C', 'D', 'F'};
+
+			for (var i = 1; i < 5; i++)
+			{
+				// Each 20% gets set the specific grade
+				if (studentGrades[(oneFifth * i) - 1] <= averageGrade)
+				{
+					return obtainableGrades[i];
+				}
+			}
+
+			return base.GetLetterGrade(averageGrade);
 		}
 	}
 }
