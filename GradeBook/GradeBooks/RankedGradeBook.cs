3154ea0b8a98ac05685e21d70ef9e4e9aa75ac3d
eyyzam <bronislawskiandrzej@gmail.com>
2020-10-15 23:13:20 +0200

    STEP - Override RankedGradeBook's CalculateStatistics method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -37,5 +37,16 @@ namespace GradeBook.GradeBooks
 
 			return base.GetLetterGrade(averageGrade);
 		}
+
+		public override void CalculateStatistics()
+		{
+			if (Students.Count < 5)
+			{
+				Console.WriteLine("Ranked grading requires at least 5 students.");
+				return;
+			}
+
+			base.CalculateStatistics();
+		}
 	}
 }
